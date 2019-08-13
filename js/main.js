9e00783fa73e8d866f2d51cd976dd7e0b531fb64
Diego Rojas <diego@d-mentecreativa.com>
2019-08-13 19:38:30 +0200

    form and Airtable connection added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 var firstPhrase = document.getElementById('first-phrase');
 var lastPhrase = document.getElementById('last-phrase');
 var content = document.getElementById('main');
+var form = document.getElementById('confirm');
+var formSuccess = document.getElementById('form-success');
+
+var airtable_endpoint = "https://api.airtable.com/v0/appJU3aBEw49QzT0b/Invite?api_key=keyieOwATKOFQswlE"
 
 lastPhrase.addEventListener('webkitAnimationEnd',function( event ) {
   firstPhrase.style.display = 'none';
   lastPhrase.style.display = 'none';
   content.classList.remove('hidden');
 }, false);
+
+form.addEventListener("submit", function(event) {
+
+  var name = document.getElementById('form-name');
+
+  event.preventDefault();
+
+  console.log(name);
+
+  axios.post(airtable_endpoint, {
+    "fields": {
+      "name": name.value,
+      "confirmed": true
+    }
+  })
+  .then(function(response) {
+    if (response.status == 200) {
+      form.classList.add('hidden');
+      formSuccess.classList.remove('hidden');
+    }
+  })
+});
